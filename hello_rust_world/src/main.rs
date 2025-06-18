fb4c626d872ef75a66833918aa7f970f73b1e0d0
Yevgen Aleksandrovich <l.jegoush@gmail.com>
2025-06-19 00:28:25 +0300

    second 1000 commit

--- hello_rust_world/src/main.rs
+++ hello_rust_world/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     println!("Hello, world!");
-    let text: String = String::from(".....after 1000  times ");
+    let text: String = String::from(".....after 10001  times ");
     println!("my name is Yevgen and this my fiqrst Rust program....{}", text);
 }
